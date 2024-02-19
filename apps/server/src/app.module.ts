6b9f813525b9fc60b2bf0affef62d80414bd9b7f
Enrique Santos <santosenrique869@gmail.com>
2024-02-19 09:19:40 -0300

    chore: add environment schema and use it for config validation

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,23 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './@modules/auth/auth.module';
 import { DatabaseModule } from './@modules/database/database.module';
 import { UsersModule } from './@modules/users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import { z } from 'zod';
+import { envSchema } from './@shared/config/env-schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validate(config) {
-        const envSchema = z.object({
-          PORT: z.coerce.number().optional().default(3000),
-          DATABASE_URL: z.string().url(),
-          JWT_SECRET: z.string(),
-        });
-
-        return envSchema.parse(config);
-      },
+      validate: (config) => envSchema.parse(config),
     }),
     DatabaseModule,
     AuthModule,
